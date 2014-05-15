30bcda8482ff2276c7f5e99c46f4aaa7142abf4e
cregan7039 <cregan7039@sohamcollege.org.uk>
2014-05-15 09:44:22 +0100

    Update diceRoller.py

--- diceRoller.py
+++ diceRoller.py
@@ -1,7 +1,27 @@
-#Coin flip program
-#Describe the purpose of this program here.
+import random #Random for dice
+import time #Imported time for
 
-inport random;time
+roll=0  #Used in tie with global variable
+
+def roll(): #Roll for dice
+    print("rolling.....")
+    global roll = random.randint(1, 6) #Error with the equals sign
+    
+
+def show_dice(roll):    #Takes roll number and prints it as a dice
+    if roll == 1:
+        print(S1)
+    elif roll == 2:
+        print(s2)
+    elif roll == 3:
+        print(s3)
+    elif roll == 4:
+        print(s4)
+    elif roll == 5:
+        print(s5)
+    elif roll == 6:
+        print(s6)
+
 
 s1 = "- - - - -\n|       |\n|   O   |\n|       |\n- - - - -\n"
 s2 = "- - - - -\n| O     |\n|       |\n|     O |\n- - - - -\n"
@@ -10,26 +30,8 @@ s4 = "- - - - -\n| O   O |\n|       |\n| O   O |\n- - - - -\n"
 s5 = "- - - - -\n| O   O |\n|   O   |\n| O   O |\n- - - - -\n"
 s6 = "- - - - -\n| O   O |\n| O   O |\n| O   O |\n- - - - -\n"
 
-def roll():
-    Print("rolling....."
-    roll = rand.randing(7)
-
-
-def show_dice(roll)
-    if roll = 1:
-        print(S1)
-    else roll = 2
-        print(s2)
-    else roll = 3
-        print(s3)
-    else roll = 4
-        print(s4)
-    else roll = 5
-        print(s5)
-    else roll = 6
-        print(s6)
-
-roll
+roll() #Runs roll function
 time.sleep(1)
-show_dice(roll)
+show_dice(roll) #Runs show_dice function
+
 
