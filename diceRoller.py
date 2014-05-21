93cf90e9a02785b11002caabb010cd8dfd71493a
cregan7039 <cregan7039@sohamcollege.org.uk>
2014-05-21 20:42:03 +0100

    Update diceRoller.py

--- diceRoller.py
+++ diceRoller.py
@@ -1,16 +1,24 @@
-import random #Random for dice
-import time #Imported time for
+import random , time
 
-roll=0  #Used in tie with global variable
+s1 = "- - - - -\n|       |\n|   O   |\n|       |\n- - - - -\n"
+s2 = "- - - - -\n| O     |\n|       |\n|     O |\n- - - - -\n"
+s3 = "- - - - -\n| O     |\n|   O   |\n|     O |\n- - - - -\n"
+s4 = "- - - - -\n| O   O |\n|       |\n| O   O |\n- - - - -\n"
+s5 = "- - - - -\n| O   O |\n|   O   |\n| O   O |\n- - - - -\n"
+s6 = "- - - - -\n| O   O |\n| O   O |\n| O   O |\n- - - - -\n"
 
-def roll(): #Roll for dice
-    print("rolling.....")
-    global roll = random.randint(1, 6) #Error with the equals sign
-    
+print("Hello there fellow human.")
+print("\nThe program runs until a 6 is rolled.")
+print("\nEnjoy yourself whilst a 6 hasnt been rolled.")
+time.sleep(2)
 
-def show_dice(roll):    #Takes roll number and prints it as a dice
+print("\nRolling... :D")
+roll = ""   #Roll as empty string
+
+while roll !=6: #Runs only if number dosent equal 6. If it does equal 6 then the loop ends
+    roll = random.randint(1,6)  #Random number gen for roll
     if roll == 1:
-        print(S1)
+        print(s1)
     elif roll == 2:
         print(s2)
     elif roll == 3:
@@ -21,17 +29,6 @@ def show_dice(roll):    #Takes roll number and prints it as a dice
         print(s5)
     elif roll == 6:
         print(s6)
-
-
-s1 = "- - - - -\n|       |\n|   O   |\n|       |\n- - - - -\n"
-s2 = "- - - - -\n| O     |\n|       |\n|     O |\n- - - - -\n"
-s3 = "- - - - -\n| O     |\n|   O   |\n|     O |\n- - - - -\n"
-s4 = "- - - - -\n| O   O |\n|       |\n| O   O |\n- - - - -\n"
-s5 = "- - - - -\n| O   O |\n|   O   |\n| O   O |\n- - - - -\n"
-s6 = "- - - - -\n| O   O |\n| O   O |\n| O   O |\n- - - - -\n"
-
-roll() #Runs roll function
-time.sleep(1)
-show_dice(roll) #Runs show_dice function
-
+        print()
+        print("The program has sadly ended...")
 
